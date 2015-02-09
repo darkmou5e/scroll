d0332905f26089e1158d8527156b0e8f52be7721
Anton Bezrukov <bezrukov.msk@gmail.com>
2015-02-09 04:46:55 +0300

    Added render function and some tests

--- src/scroll/core.clj
+++ src/scroll/core.clj
@@ -1,6 +1,38 @@
-(ns scroll.core)
+(ns scroll.core
+  (:require [clojure.string :as -s]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn html [& elements]
+
+  (defn render-inner [i]
+    (if (seq? i) (-s/join (map html i)) ""))   
+
+  (defn render-attrs [a]
+    (defn render-attr [[a v]]
+      (cond
+        (string? v) (str " " (name a) \= \" v \")
+        :else (if v (str " " (name a)))))
+      (-s/join (map render-attr a)))  
+
+  (defn render-html [t a i] (str \< t a \> i \<\/ t \>))
+
+  (defn render-tag [e]
+    (let [tag (name (first e))
+          attrs-inner (seq (rest e))]
+      (if (map? (first attrs-inner))
+        (render-html tag
+                     (render-attrs (first attrs-inner))
+                     (render-inner (rest attrs-inner)))
+        (render-html tag
+                     ""
+                     (render-inner attrs-inner)))))
+
+  (defn render [element]
+    (cond
+      (string? element) element
+      (vector? element) (render-tag element)
+      (seq? element) (-s/join (map render element))))
+
+  (-s/join (map render elements)))
+
+
+
--- test/scroll/core_test.clj
+++ test/scroll/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [scroll.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest all-in-one-with-spam
+    (is (= (html [:div]) "<div></div>"))
+    (is (= (html [:div "Hello!"]) "<div>Hello!</div>"))
+    (is (= (html [:div {:class "header"}]) "<div class=\"header\"></div>"))
+    (is (= (html [:div {:class "header" :data-spam "eggs"}]) "<div class=\"header\" data-spam=\"eggs\"></div>"))
+    (is (= (html [:input {:type "checkbox" :checked true}]) "<input type=\"checkbox\" checked></input>"))
+    (is (= (html [:input {:type "checkbox" :checked false}]) "<input type=\"checkbox\"></input>"))    
+    ;;  (is (= (html [:input {:type "checkbox" :checked true}]) "<input type=\"checkbox\" checked>")) + <br/> + <p></p> = <p/> etc...    
+    ;; + sanitize?
+    (is (= (html [:div {:class "header" :data-spam "eggs"} "Want some spam?"]
+                 "spam"
+                 " and spam"
+                 [:p "and spam"]
+                 "and spam with spam")
+           "<div class=\"header\" data-spam=\"eggs\">Want some spam?</div>spam and spam<p>and spam</p>and spam with spam"))    
+)
